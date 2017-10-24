adb71dd7414bbe23c3fda3ff08e1c8a05e6137eb
Line Zakariassen <line.zakariassen@kantega.no>
2017-10-24 10:28:54 +0200

    #3: Added some helpful comments. Made link open in new tab.

--- src/main/webapp/panda/panda.js
+++ src/main/webapp/panda/panda.js
@@ -24,29 +24,31 @@ window.addEventListener("load", function () {
 
 
     function drawWhiteBackground() {
-        g.fillStyle = "white"
-        g.beginPath()
-        g.arc(250, 165, 30, 0, 0.9 * Math.PI)
-        g.fill()
+        g.fillStyle = "white";
+        g.beginPath();
+        g.arc(250, 165, 30, 0, 0.9 * Math.PI);
+        g.fill();
     }
 
     function drawMouth() {
-
-        g.lineWidth = 2
-        g.beginPath()
-        g.arc(255, 195, 20, 1.1 * Math.PI, 1.8 * Math.PI)
-        g.stroke()
+        g.lineWidth = 2;
+        g.beginPath();
+        //TODO: Make the panda smile, tip: .arc(x, y, radius, startAngle, endAngle)
+        g.arc(255, 195, 20, 1.1 * Math.PI, 1.8 * Math.PI);
+        //TODO: Set mouth color
+        g.stroke();
     }
 
 
     function drawHat() {
-        g.fillStyle = "yellow"
+        g.fillStyle = "yellow";
         g.beginPath();
+        //TODO: Make the hat sit on top of the panda's head.
         g.moveTo(100, 100);
         g.lineTo(0, 50);
         g.lineTo(100, 0);
         g.fill();
-        g.strokeStyle = "green"
+        g.strokeStyle = "green";
         g.stroke()
     }
 
